9f6e2e58a84c2b6ce7f90b0d2b0d14372df185d8
bee712 <48711413+bee712@users.noreply.github.com>
2022-06-09 17:15:03 +0900

    Create PullLeverGameView

---
+++ MC2-Team1/PullLeverGameView.swift
@@ -0,0 +1,20 @@
+//
+//  PullLeverGameView.swift
+//  MC2-Team1
+//
+//  Created by Eunbi Han on 2022/06/09.
+//
+
+import SwiftUI
+
+struct PullLeverGameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PullLeverGameView_Previews: PreviewProvider {
+    static var previews: some View {
+        PullLeverGameView()
+    }
+}
