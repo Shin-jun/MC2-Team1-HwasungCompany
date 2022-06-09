3305d2796e09fdd1f3a06ad13851886c9adfef67
jeonghyeon Hwang <hijihilin@gmail.com>
2022-06-09 17:21:27 +0900

    [FEAT] Add HapticManager (#1)

---
+++ MC2-Team1/HapticManager.swift
@@ -0,0 +1,24 @@
+//
+//  HapticManager.swift
+//  MC2-Team1
+//
+//  Created by 황정현 on 2022/06/09.
+//
+
+import SwiftUI
+
+class HapticManager {
+    
+    static let instance = HapticManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
